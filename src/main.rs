4ac083012322e606ec9d7907f8a8106b27107efc
John W <112655410+jozhw@users.noreply.github.com>
2023-11-16 22:27:03 -0600

    enable cli functionalities

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,52 @@
+use std::path::PathBuf;
+
 use clap::Parser;
 
-use stdirgen::Cli;
+use stdirgen::{cli::EntityType, create_dir, create_file, create_subfiles, Cli};
 
 fn main() {
-    let _args = Cli::parse();
+    let cli = Cli::parse();
+    match &cli.entity_type {
+        EntityType::Directory(args) => {
+            let entity_type = EntityType::Directory(args.clone());
+            let dir_name: &str = entity_type.get_name();
+            let path: PathBuf = entity_type.get_path();
+            let dir_iter: &i32 = entity_type.get_iter();
+            let dir_start: &i32 = entity_type.get_start();
+            let subfile_commands: Option<stdirgen::cli::SubfileCommands<'_>> =
+                entity_type.get_subfile_commands();
+
+            let enumer_dirs: Vec<String> =
+                create_dir::create_dir(dir_name, &path, &dir_iter, &dir_start).unwrap();
+
+            match subfile_commands {
+                Some(command) => {
+                    let files: &Vec<String> = command.files;
+                    let files_iter: &Vec<i32> = command.files_iter;
+                    let files_start: &Vec<i32> = command.files_start;
+
+                    for directory in enumer_dirs {
+                        let mut dir_path = path.clone();
+                        dir_path.push(directory);
+
+                        create_subfiles::create_subfiles(files, files_iter, files_start, &dir_path)
+                            .unwrap();
+                    }
+                }
+                None => {}
+            }
+
+            println!("The values of directory command is {:?}", dir_name)
+        }
+
+        EntityType::File(args) => {
+            let entity_type: EntityType = EntityType::File(args.clone());
+            let file_name: &str = entity_type.get_name();
+            let path: PathBuf = entity_type.get_path();
+            let file_iter: &i32 = entity_type.get_iter();
+            let file_start: &i32 = entity_type.get_start();
+
+            create_file::create_file(file_name, &path, file_iter, file_start).unwrap();
+        }
+    }
 }
