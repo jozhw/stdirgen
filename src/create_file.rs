76ffc23dfabc6105ec6a437a007f3e552aba8913
John W <112655410+jozhw@users.noreply.github.com>
2023-11-16 21:59:11 -0600

    add: typing of variables and function comments

--- src/create_file.rs
+++ src/create_file.rs
@@ -5,14 +5,25 @@ use std::path::PathBuf;
 use crate::errors::create_error::CreateError;
 use crate::utils::split_string::{split_string, SplitResult};
 
-// need name, path, iter, start
+/// create_files creates files from a file name
+///
+/// # Arguments
+///
+/// * 'name' - A file name to create that must contain the delimiter ".".
+/// * 'path' - Path to where the files will reside.
+/// * 'iter' - How many iterations of enumeration.
+/// * 'start' - The starting value of the enumeration.
+///
+/// # Returns
+///
+/// Result type.
 pub fn create_file(
     name: &str,
     path: &PathBuf,
     iter: &i32,
     start: &i32,
 ) -> Result<(), Box<dyn Error>> {
-    let end = iter + start;
+    let end: i32 = iter + start;
     let delimiter: char = '.';
 
     // split the file extension from name and concat after numeration occurs
@@ -59,27 +70,27 @@ mod tests {
 
     #[test]
     fn test_create_file() {
-        let name = "test_file.txt";
-        let path = PathBuf::from("./target");
-        let iter = 2;
-        let start = 1;
+        let name: &str = "test_file.txt";
+        let path: PathBuf = PathBuf::from("./target");
+        let iter: i32 = 2;
+        let start: i32 = 1;
         // test case
-        let test_name = "test_file";
-        let test_ext = "txt";
-        let delimiter = ".";
+        let test_name: &str = "test_file";
+        let test_ext: &str = "txt";
+        let delimiter: &str = ".";
         // must fail path
 
-        let result = create_file(name, &path, &iter, &start);
+        let result: Result<(), Box<dyn Error>> = create_file(name, &path, &iter, &start);
 
         assert!(result.is_ok(), "Failed to create files {:?}", result);
 
         // confirm files were created
         for i in start..start + iter {
-            let file_name = format!("{}{}{}{}", test_name, i, delimiter, test_ext);
-            let mut complete_path = path.clone();
+            let file_name: String = format!("{}{}{}{}", test_name, i, delimiter, test_ext);
+            let mut complete_path: PathBuf = path.clone();
             complete_path.push(file_name);
 
-            let mut fail_complete_path = path.clone();
+            let mut fail_complete_path: PathBuf = path.clone();
             let test_file_name = format!("{}{}{}", test_name, i, test_ext);
 
             // must fail because file name is missing delimiter
@@ -96,16 +107,13 @@ mod tests {
                 complete_path
             )
         }
-
         // remove test files
         for i in start..start + iter {
-            let file_name = format!("{}{}{}{}", test_name, i, delimiter, test_ext);
-            let mut complete_path = path.clone();
+            let file_name: String = format!("{}{}{}{}", test_name, i, delimiter, test_ext);
+            let mut complete_path: PathBuf = path.clone();
             complete_path.push(file_name);
 
             let _ = std::fs::remove_file(&complete_path);
         }
     }
 }
-
-
